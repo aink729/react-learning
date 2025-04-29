acd1d2b128fdc11cff52c7c1bda5e6e1c937ec60
dev-aaaa <127377783+dev-aaaa@users.noreply.github.com>
2025-04-29 17:13:55 +0900

    feat: 카드 데이터 확장 및 Grid 레이아웃 적용

--- card-list/src/components/Card.jsx
+++ card-list/src/components/Card.jsx
@@ -1,10 +1,13 @@
 import './Card.css'; // css 파일 불러오기
 
-export default function Card({ title, content }) {
+export default function Card({ title, content, price, imageUrl }) {
     return (
         <div className="card">
+            <img src={imageUrl} alt={title} className="card-image" />
             <h2>{title}</h2>
             <p>{content}</p>
+            <p className="price">{price}</p>
+            <button className="buy-button">구매하기</button>
         </div>
     )
 }
--- card-list/src/components/CardList.jsx
+++ card-list/src/components/CardList.jsx
@@ -3,16 +3,26 @@ import './CardList.css';
 
 
 const data = [
-    { id: 1, title: "첫 번째 카드", content: "이건 첫 번째 카드입니다."},
-    { id: 2, title: "두 번째 카드", content: "두 번째 카드"},
-    { id: 3, title: "세 번째 카드", content: "이건 세 번째 카드입니다."},
+    { id: 1, title: "첫 번째 상품", content: "이건 첫 번째 상품입니다.", price: "₩29,000", imageUrl: "https://placehold.co/300x200" },
+  { id: 2, title: "두 번째 상품", content: "이건 두 번째 상품입니다.", price: "₩49,000", imageUrl: "https://placehold.co/300x200" },
+  { id: 3, title: "세 번째 상품", content: "이건 세 번째 상품입니다.", price: "₩19,000", imageUrl: "https://placehold.co/300x200" },
+  { id: 4, title: "네 번째 상품", content: "이건 네 번째 상품입니다.", price: "₩59,000", imageUrl: "https://placehold.co/300x200" },
+  { id: 5, title: "다섯 번째 상품", content: "이건 다섯 번째 상품입니다.", price: "₩99,000", imageUrl: "https://placehold.co/300x200" },
+  { id: 6, title: "여섯 번째 상품", content: "이건 여섯 번째 상품입니다.", price: "₩15,000", imageUrl: "https://placehold.co/300x200" }
+
 ];
 
 export default function CardList() {
     return (
         <div className="catd-list">
             {data.map((item) => (
-                <Card key={item.id} title={item.title} content={item.content} />
+                <Card 
+                    key={item.id} 
+                    title={item.title} 
+                    content={item.content} 
+                    imageUrl={item.imageUrl}
+                    price={item.price}
+                />
             ))}
         </div>
     )
