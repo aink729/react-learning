7dae4ef86ffaca66f34c2fbb67ed93ebbfb0b983
aink729 <listentolion@naver.com>
2025-05-12 17:41:01 +0900

    feat: 카드 클릭 시 상세 페이지로 이동 기능추가

--- react-shop/src/pages/DetailPage.jsx
+++ react-shop/src/pages/DetailPage.jsx
@@ -1,3 +1,16 @@
+import { useParams } from "react-router-dom";
+import styles from "./DetailPage.module.css"
+
 export default function DetailPage() {
-    return <div>상세 페이지</div>;
-  }
+  const { id } = useParams(); 
+  
+  return(
+    <div className={styles['sub-layout']}>
+      <div className={styles['sub-inner']}>
+        <h1>{id}번 상품 상세 페이지</h1>
+        { /* 나중에 product.json에서 Id로 해당 상품 찾아서 보여줘도 돼요! */ }
+      </div>
+    </div>
+  )
+
+}
