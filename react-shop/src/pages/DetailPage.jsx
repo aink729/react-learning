74163ad894e56c9b729804483170881a97727918
aink729 <listentolion@naver.com>
2025-05-13 11:59:51 +0900

    feat: 추천 상품 id 공통 변수로 추가

--- react-shop/src/pages/DetailPage.jsx
+++ react-shop/src/pages/DetailPage.jsx
@@ -1,14 +1,38 @@
-import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import { getRandomItems } from "../utils/getRandomItems";
 import products from "../data/products.json";
 import styles from "./DetailPage.module.css";
 
 export default function DetailPage() {
-  const { id } = useParams();  // 경로 파라미터 받아오기
-  const product = products.find((item) => item.id === parseInt(id)); // item.id 숫자(Number), parseInt(id) 숫자로 변환
-  const recommended = products.filter((item) => item.id !== Number(id)).slice(0,4);
-  // 현재 상품과 같은 카테고리이지만, 현재 상품은 제외
-  const recommendedCate = products.filter((item) => item.category === product.category && item.id !== product.id)
+  const { id } = useParams();  // 경로 파라미터 받아오기. 항상 문자열로 반환함. "숫자"
+  const productId = Number(id); // 또는 parseInt(id)
+  const product = products.find((item) => item.id === productId); // item.id 숫자(Number), parseInt(id) 숫자로 변환
+
+  // const recommended = products.filter((item) => item.id !== Number(id)).slice(0,4);
+  // 랜덤으로 4개 추출
+  // const recommended = products
+  // .filter((item) => item.id !== Number(id)) // 현재 상품은 제외
+  // .sort(() => Math.random() - 0.5 ) // 🔀 배열 섞기 (무작위 정렬)
+  // .slice(0,4) // ✂️ 앞에서 4개만 추출
+
+  // 추천 상품 (현재 상품 제외 후 랜덤 4개)
+  const recommended = getRandomItems(
+    products.filter((item) => item.id !== productId),
+    4
+  )
+
+  // 현재 상품과 같은 카테고리이지만, 현재 상품은 제외, 랜덤으로 4개 추출
+  // const recommendedCate = products
+  // .filter((item) => item.category === product.category && item.id !== product.id)
+  // .sort(() => Math.random() - 0.5 ) // 🔀 배열 섞기 (무작위 정렬)
+  // .slice(0,4) // ✂️ 앞에서 4개만 추출
+
+
+  // 추천 상품 (현재 상품과 같은 카테고리이지만 현재 상품 제외 후 랜덤 4개)
+  const recommendedCate = getRandomItems(
+    products.filter((item) => item.category === product.category  && item.id !== productId),
+    4
+  )
 
   // id가 없는 페이지를 열 경우
   if(!product) {
@@ -61,9 +85,15 @@ export default function DetailPage() {
           <ul className={styles.recommendList}>
             {recommendedCate.map((item) => (
               <li key={item.id} className={styles.recommendItem}>
-                <img src={item.imageUrl} alt={`${item.title} 이미지`} />
-                <p>{item.title}</p>
-                <p>{item.price.toLocaleString()}원</p>
+                <Link
+                  key={item.id}
+                  to={`/detail/${item.id}`}
+                  className={styles.recommendLink}
+                >
+                  <img src={item.imageUrl} alt={`${item.title} 이미지`} />
+                  <p>{item.title}</p>
+                  <p>{item.price.toLocaleString()}원</p>
+                </Link>
               </li>
             ))}
           </ul>
