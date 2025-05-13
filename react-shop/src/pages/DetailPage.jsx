caf51a58e3ccb13161efdbf012d498bb27955b07
aink729 <listentolion@naver.com>
2025-05-13 10:22:52 +0900

    feat: 상세 페이지에서 상품 정보 조회 및 레이아웃 구현

--- react-shop/src/pages/DetailPage.jsx
+++ react-shop/src/pages/DetailPage.jsx
@@ -1,13 +1,36 @@
 import { useParams } from "react-router-dom";
-import styles from "./DetailPage.module.css"
+import products from "../data/products.json";
+import styles from "./DetailPage.module.css";
 
 export default function DetailPage() {
-  const { id } = useParams(); 
+  const { id } = useParams();  // 경로 파라미터 받아오기
+  const product = products.find((item) => item.id === parseInt(id)); 
+
+  if(!product) {
+    return (
+      <div className={styles['sub-layout']}>
+        <div className={styles['sub-inner']}>
+          상품을 찾을 수 없습니다.
+        </div>
+      </div>
+    );
+  }
   
   return(
     <div className={styles['sub-layout']}>
       <div className={styles['sub-inner']}>
-        <h1>{id}번 상품 상세 페이지</h1>
+        <div className={styles.info}>
+          <img className={styles.img} src={product.imageUrl} alt={`${product.title} 이미지`} />
+          <h2>{product.title}</h2>
+          <p>{product.content}</p>
+          <p className={styles.price}><strong></strong></p>
+          {product.soldOut ? (
+              <p className={styles.soldOut}>❌ 품절된 상품입니다</p>
+            ) : (
+              <button className={styles.buyButton}>구매하기</button>
+            )
+          }
+        </div>
         { /* 나중에 product.json에서 Id로 해당 상품 찾아서 보여줘도 돼요! */ }
       </div>
     </div>
